e1270be8c9214c9a6244f90488d31567887fa6b3
proveedor_hhurtado <proveedor_hhurtado@CO-D-2071MN9.ath.net>
2016-06-01 11:22:13 -0500

    Adición del proyecto Commons

---
+++ src/main/java/com/dataconnector/constans/DataConnectorConstants.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.dataconnector.constans;
+
+/**
+ *{Insert class description here}
+ * @version $Revision: 1.1.1  (UTF-8)
+ * @since build 2/05/2016  
+ * @author proveedor_hhurtado  email: proveedor_hhurtad@ath.com.co
+ */
+public class DataConnectorConstants {
+
+    public static final  String DRIVER_MYSQL="com.mysql.jdbc.Driver";
+    public static final String DRIVER_ORACLE="oracle.jdbc.driver.OracleDriver";
+    public static  final String DRIVER_SQLSERVER="com.microsoft.sqlserver.jdbc.SQLServerDriver";
+}
---
+++ src/main/java/com/dataconnector/constans/ProvidersSupportEnum.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dataconnector.constans;
+
+/**
+ * Enum que representa los tipos de driver especializados por dataconnector
+ *
+ * @version $Revision: 1.1.1 (UTF-8)
+ * @since build 23/02/2016
+ * @author proveedor_hhurtado email: proveedor_hhurtad@ath.com.co
+ */
+public enum ProvidersSupportEnum {
+    GENERIC,ORACLE,SQLSERVER;
+}
